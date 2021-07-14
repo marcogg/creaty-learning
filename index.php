635a756100167247a9287f350cc74f9205e4dcc9
Marco Garcia <marcogarcia.gon@gmail.com>
2021-07-14 12:36:20 -0500

    commit inicial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- STYLES -->
+    <link rel="stylesheet" href="css/simple-grid/grid/simple-grid.min.css">
+    <title>Creaty Leraning | Cursos digitales para emprendedores y Freelancers </title>
+</head>
+<body>
+    <header>
+        <nav class="nav">
+        	<li><a href="#cursos">Cursos</a></li>
+        	<li><a href="#testimonios">Testimonios</a></li>
+        	<li><a href="#reserva">Reserva tu lugar</a></li>
+        </nav>
+    </header>
+    <main>
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-6">
+    				<h3>Aprende a hacer e impulsar tu sitio web</h3>
+    			</div>
+    			<div class="col-6">
+    				<img src="https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/vimdb/202685_0-803-5447-5447.jpg">
+    			</div>
+    			
+    		</div>
+    		<div class="row">
+    			
+    		</div>
+    	</div>
+    </main>
+</body>
+</html>
