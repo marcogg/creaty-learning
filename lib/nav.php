d8b0438142150ce029a11acd871a5c2d80746d8b
Marco Garcia <marcogarcia.gon@gmail.com>
2022-02-24 20:49:40 -0600

    Cambio a HTML

---
+++ lib/nav.php
@@ -0,0 +1,8 @@
+<nav class="nav">
+    <ul>
+        <li><a href="index.html#acerca" class="nav-link">Acerca de</a></li>
+        <li><a href="index.html#curso" class="nav-link">Conoce el curso</a></li>
+        <li><a href="index.html#testimonios" class="nav-link">Testimonios</a></li>
+        <li><a href="index.html#reserva" class="btn btn-cta">Reserva tu lugar</a></li> 
+    </ul>
+</nav>
